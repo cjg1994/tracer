b4c5b169740f6a498fc8978857d7a5357763463a
cjg1994 <45484062+cjg1994@users.noreply.github.com>
2021-08-09 14:37:33 +0800

    test git

--- tracer/manage.py
+++ tracer/manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    test = "test"
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracer.settings")
     try:
         from django.core.management import execute_from_command_line
